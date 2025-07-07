aa407e7586286f3f6ce32c684c75128a4bdbd806
masoudkaarimi <masoud.develop@gmail.com>
2025-07-07 23:59:49 +0330

    Bug fixed

--- loan-management-system/src/core/wsgi.py
+++ loan-management-system/src/core/wsgi.py
@@ -11,15 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from .settings.base import DEBUG
-
-if DEBUG:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
-    print("WSGI: Django loaded up in setting mode : \033[1;34mDevelopment\033[0m")
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
-    print("WSGI: Django loaded up in setting mode : \033[1;34mProduction\033[0m")
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.development")
 
 application = get_wsgi_application()
